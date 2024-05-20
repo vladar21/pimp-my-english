8e1dbd27c03d1191f53efa1a7c2d85d5e6930d08
vladar21 <vladar21@gmail.com>
2024-05-21 00:09:47 +0100

    feat: create django project pimp_my_english

---
+++ pimp_my_english/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pimp_my_english project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pimp_my_english.settings')
+
+application = get_wsgi_application()
