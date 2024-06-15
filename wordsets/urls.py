8eaeccacc7529bbd24ef44751a287d1b97be88bb
vladar21 <vladar21@gmail.com>
2024-06-15 10:52:39 +0100

    feat: update wordsets/urls.py

--- wordsets/urls.py
+++ wordsets/urls.py
@@ -7,8 +7,8 @@ app_name = 'wordsets'
 
 urlpatterns = [
     path('', views.list_word_sets, name='list_word_sets'),
-    path('create/', views.create_word_set, name='create_word_set'),
-    path('<int:word_set_id>/update/', views.update_word_set, name='update_word_set'),
     path('<int:word_set_id>/words/', views.get_word_set_words, name='get_word_set_words'),
     path('<int:word_set_id>/delete/', views.delete_word_set, name='delete_word_set'),
+    path('create/', views.create_or_update_word_set, name='create_word_set'),
+    path('<int:word_set_id>/update/', views.create_or_update_word_set, name='update_word_set'),
 ]
