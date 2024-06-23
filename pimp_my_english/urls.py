7c6965c988b1106f0409eb1a3074165715422f29
vladar21 <vladar21@gmail.com>
2024-06-23 23:20:50 +0100

    feat: add privacy policy and terms and conditions pages

--- pimp_my_english/urls.py
+++ pimp_my_english/urls.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import landing
+from .views import PrivacyPolicyView, TermsAndConditionsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +13,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('allauth.urls')),
     path('subscriptions/', include('subscriptions.urls')),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy_policy'),
+    path('terms-and-conditions/', TermsAndConditionsView.as_view(), name='terms_and_conditions'),
 ]
--- pimp_my_english/views.py
+++ pimp_my_english/views.py
@@ -1,7 +1,16 @@
 # pimp_my_english/views.py
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def landing(request):
     return render(request, 'landing.html')
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = "privacy_policy.html"
+
+
+class TermsAndConditionsView(TemplateView):
+    template_name = "terms_and_conditions.html"
